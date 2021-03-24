ec8d5d9b0dc17e5860ad75d6a6511c191ebf00c7
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2021-03-24 17:23:53 -0300

    modificações concluidas

--- backend/src/classes/handles/userhandle.php
+++ backend/src/classes/handles/userhandle.php
@@ -5,20 +5,17 @@ header("Access-Control-Allow-Headers: *");
 //require_once("../../../vendor/autoload.php"); 
 
 use connection\Sql;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
 class userhandle {
     private $sql;
 
-    public function ALL(Request $request, Response $response){
+    public function ALL(){
         $this->sql = new sql();
-        
-        $stmt = $this->sql -> comand(
+        $test = $this->sql -> comand(
         "SELECT * FROM substancias"
         );
-        $response->getBody()->write($stmt);
-        return $response;
+
+        return var_dump($test);
     } 
 }
 ?>
