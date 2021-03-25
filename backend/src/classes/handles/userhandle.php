2c85a23164362f83318f9e2634ce39e6f17bdb0e
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2021-03-25 20:53:18 -0300

    Modificações feitas

--- backend/src/classes/handles/userhandle.php
+++ backend/src/classes/handles/userhandle.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Headers: *");
 //require_once("../../../vendor/autoload.php"); 
 
 use connection\Sql;
+use Slim\Http\StatusCode;
 
 class userhandle {
     private $sql;
@@ -12,10 +13,49 @@ class userhandle {
     public function register($req){
         $this->sql = new Sql();
         $body = json_decode($req);
-        $data = $this->sql -> comandFetch(
-            "INSERT INTO users (senha , email) VALUES ($body->senha , $body->email)"
+        if($this->sql->comandFetch(
+         "SELECT * FROM users WHERE '$body->email' OR '$body->senha'"   
+        )){
+            return false;
+        }else{
+            $this->sql -> comand(
+            "INSERT INTO users (senha , email) VALUES ('$body->senha' , '$body->email')"
+            );
+            return $req;
+        }
+    }
+    public function login($req){
+        $this->sql = new Sql();
+        $body = json_decode($req);
+        $check = $this->sql->comandFetch(
+            "SELECT * FROM users WHERE email='$body->email' AND senha='$body->senha' "
         );
-        return json_encode($data);
+        if($check){
+            $result = json_encode($check);
+        }else{
+            $result = false;
+        }
+        return $result;
+    }
+    public function salvar($req , $params){
+        //vai pegar o array para poder salvar a droga , talvez criar pivate funciton
+        $this->sql = new Sql();
+        $body = json_decode($req);
+
+    }
+    public function desalvar($req , $params){
+        //vai tirar do array para poder salvar a droga , talvez criar pivate function
+        $this->sql = new Sql();
+        $body = json_decode($req);
+    }
+    public function verGrafico($req){
+        //vai ter q criar uma nova tabela ,  que vai buscar os dados e alterar
+        $this->sql = new Sql();
+        $body = json_decode($req);
+    }
+    public function alterarGrafico($req){
+        $this->sql = new Sql();
+        $body = json_decode($req);
     }
 }
 ?>
--- backend/src/public/index.php
+++ backend/src/public/index.php
@@ -20,10 +20,44 @@ $drogas = new drugshandle();
 $app->post('/register' , function(Request $request, Response $response){
     global $user;
     $body = $request->getBody();
-    //$data = json_decode($body);
     $stmt = $user->register($body);
-    $response->getBody()->write($stmt);
+    if($stmt == false){
+        $response->withStatus(400)->getBody()->write('Conta ja criada');
+    }else{
+        $response->getBody()->write($stmt);
+    }
     return $response;
+});
+$app->post('/logon' , function(Request $request , Response $response){
+    global $user;
+    $body = $request->getBody();
+    $stmt = $user->login($body);
+    if($stmt == false){
+        return $response->withStatus(400)->getBody()->write('Not permited');
+    }else{
+        $response->getBody()->write($stmt);
+    }
+    return $response;
+});
+$app->post('/salvar/:id',function(Request $request ,Response $response , $id){
+    global $user;
+    $body = $request->getBody();
+    $stmt = $user->salvar($body , $id);
+});
+$app->post('/desalvar/:id',function(Request $request , Response $response , $id){
+    global $user;
+    $body = $request->getBody();
+    $stmt = $user->desalvar($body , $id);
+});
+$app->get('/grafico',function(Request $request , Response $response){
+    global $user;
+    $body = $request->getBody();
+    $stmt = $user->verGrafico($body);
+});
+$app->post('/editGrafico',function(Request $request , Response $response){
+    global $user;
+    $body = $request->getBody();
+    $stmt = $user->alterarGrafico($body);
 });
 
 $app->get('/drogas', function (Request $request, Response $response) {
