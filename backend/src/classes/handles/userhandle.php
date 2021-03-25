8d119498c405f4f8c57c149811b7635457e50374
Matheus Perozini Magalhães <perozinimagalhesmatheus@gmail.com>
2021-03-25 10:46:10 -0300

    Modificações realizadas

--- backend/src/classes/handles/userhandle.php
+++ backend/src/classes/handles/userhandle.php
@@ -8,5 +8,14 @@ use connection\Sql;
 
 class userhandle {
     private $sql;
+
+    public function register($req){
+        $this->sql = new Sql();
+        $body = json_decode($req);
+        $data = $this->sql -> comandFetch(
+            "INSERT INTO users (senha , email) VALUES ($body->senha , $body->email)"
+        );
+        return json_encode($data);
+    }
 }
 ?>
--- backend/src/public/index.php
+++ backend/src/public/index.php
@@ -2,13 +2,12 @@
 namespace src;
 
 header("Access-Control-Allow-Headers: *");
-require("../vendor/autoload.php");
+require __DIR__ . '/../vendor/autoload.php';
 
 use handles\drugshandle;
 use handles\substanceshandle;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 use handles\userhandle;
 
 $app = new \Slim\app;
@@ -17,6 +16,15 @@ $app = new \Slim\app;
 $user = new userhandle();
 $substancias = new substanceshandle();
 $drogas = new drugshandle();
+
+$app->post('/register' , function(Request $request, Response $response){
+    global $user;
+    $body = $request->getBody();
+    //$data = json_decode($body);
+    $stmt = $user->register($body);
+    $response->getBody()->write($stmt);
+    return $response;
+});
 
 $app->get('/drogas', function (Request $request, Response $response) {
     global $drogas;
